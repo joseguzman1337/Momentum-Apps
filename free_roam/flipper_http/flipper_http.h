0bf9f9cdced9e5b5fd8723dcd5f86f257acb8861
WillyJL <me@willyjl.dev>
2025-08-24 03:15:57 +0200

    Free Roam: Momentum tweaks

--- free_roam/flipper_http/flipper_http.h
+++ free_roam/flipper_http/flipper_http.h
@@ -13,6 +13,7 @@
 #include <furi_hal_gpio.h>
 #include <furi_hal_serial.h>
 #include <storage/storage.h>
+#include <momentum/settings.h>
 
 #ifdef __cplusplus
 extern "C"
@@ -21,7 +22,7 @@ extern "C"
 
 #define HTTP_TAG "FlipperHTTP"            // change this to your app name
 #define http_tag "free_roam"              // change this to your app id
-#define UART_CH (FuriHalSerialIdUsart)    // UART channel
+#define UART_CH (momentum_settings.uart_esp_channel)    // UART channel
 #define TIMEOUT_DURATION_TICKS (5 * 1000) // 5 seconds
 #define BAUDRATE (115200)                 // UART baudrate
 #define RX_BUF_SIZE 2048                  // UART RX buffer size
